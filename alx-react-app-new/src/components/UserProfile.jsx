cc8ed6983ef0bdca1f31a0e679a005840111cb45
Cyrus Maina <77388640+Cyr0707@users.noreply.github.com>
2025-11-09 20:39:18 +0300

    Update UserProfile.jsx

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 const UserProfile = (props) => {
     return (
         <div style={{ border: '1px solid #ccc', padding: '15px', margin: '20px', borderRadius: '8px', boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.1)' }}>
-            <h2 style={{ color: '#007bff', fontSize: '1.8em', marginBottom: '10px' }}>{props.name}</h2>
+            <h2 style={{ color: 'blue', fontSize: '1.8em', marginBottom: '10px' }}>{props.name}</h2>
             <p style={{ marginBottom: '5px' }}>
                 <span style={{ fontWeight: 'bold', color: '#555' }}>Age:</span> 
                 <span style={{ marginLeft: '5px', color: '#333' }}>{props.age}</span>
